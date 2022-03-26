9ec9d7563a69b8a11dd5beafe5a0bdd209d56795
L2HYUNN <dhl9810@naver.com>
2022-03-27 01:14:21 +0900

    Complete server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import socketIO from "socket.io";
+import SocketIO from "socket.io";
 import "dotenv/config";
 
 const app = express();
@@ -13,8 +13,24 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = socketIO(httpServer);
+const wsServer = SocketIO(httpServer);
 
-app.listen(process.env.PORT, () =>
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("info_enter_room", roomName);
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
+
+httpServer.listen(process.env.PORT, () =>
   console.log("Server Connected on port 'http://localhost:3000'")
 );
